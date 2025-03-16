291512da819e63f5890f0ff95080e07968d54f34
luoye20230624 <137587181+luoye20230624@users.noreply.github.com>
2025-03-16 19:39:50 +0800

    Update hook-cv2.py

--- hooks/hook-cv2.py
+++ hooks/hook-cv2.py
@@ -1,5 +1,15 @@
-from PyInstaller.utils.hooks import collect_dynamic_libs
+# hooks/hook-cv2.py
+from PyInstaller.utils.hooks import collect_dynamic_libs, collect_data_files
 
-# 自动包含所有OpenCV的DLL
+# 包含所有OpenCV的DLL
 binaries = collect_dynamic_libs('cv2')
-hiddenimports = ['numpy.core.multiarray']
+
+# 包含所有NumPy的隐藏依赖
+hiddenimports = [
+    'numpy.core._multiarray_umath',
+    'numpy.core._dtype_ctypes',
+    'numpy.core._methods'
+]
+
+# 包含NumPy的二进制文件
+datas = collect_data_files('numpy')
